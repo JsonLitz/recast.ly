8cf89d13eb37f27bdc19fe6e3e47069b79d3d7bc
Hack Reactor Student <student@hackreactor.com>
2017-07-19 22:09:22 -0700

    change app.jsx from stateless to stateful component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,65 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search />
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div>
-          <VideoPlayer video = {exampleVideoData}/>
+class App extends React.Component {
+  constructor(props){
+    super(props);
 
+  };
+
+  render(){
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <Search />
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <div>
+            <VideoPlayer video = {exampleVideoData}/>
+          </div>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos = {exampleVideoData}/>
         </div>
       </div>
-      <div className="col-md-5">
-        <VideoList videos = {exampleVideoData}/>
-      </div>
     </div>
-  </div>
-);
+  );
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <Search />
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <div>
+//           <VideoPlayer video = {exampleVideoData}/>
+//
+//         </div>
+//       </div>
+//       <div className="col-md-5">
+//         <VideoList videos = {exampleVideoData}/>
+//       </div>
+//     </div>
+//   </div>
+// );
 
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
